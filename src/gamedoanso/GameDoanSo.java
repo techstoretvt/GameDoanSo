3a27f03b69edd497c6740623060ba3bbd5b5f665
techstoretvt <techstoretvt2565@gmail.com>
2024-08-18 18:40:35 +0700

    first commit

---
+++ src/gamedoanso/GameDoanSo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gamedoanso;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tranv
+ */
+public class GameDoanSo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new TrangChu();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+    }
+    
+}
